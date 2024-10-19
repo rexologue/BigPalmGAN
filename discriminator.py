230f6cc7ebff06561c3c1fe33e83a5f309edce51
gregorymelekhov <mironovigoroffical@gmail.com>
2024-10-19 10:33:53 +0300

    .

--- discriminator.py
+++ discriminator.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.utils.spectral_norm as spectral_norm
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils import spectral_norm
+
 class Discriminator(nn.Module):
     def __init__(self, resolution, num_classes, dropout_prob=0.3):
         super(Discriminator, self).__init__()
@@ -13,33 +18,33 @@ class Discriminator(nn.Module):
 
         self.model = nn.Sequential(
             spectral_norm(nn.Conv2d(4, 64, kernel_size=4, stride=2, padding=1)),
-            nn.LayerNorm(64),
+            nn.LayerNorm([64, resolution // 2, resolution // 2]),  # Исправлено
             nn.LeakyReLU(0.2),
             nn.Dropout(dropout_prob),
 
             spectral_norm(nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)),
-            nn.LayerNorm(128),
+            nn.LayerNorm([128, resolution // 4, resolution // 4]),  # Исправлено
             nn.LeakyReLU(0.2),
             nn.Dropout(dropout_prob),
 
             spectral_norm(nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)),
-            nn.LayerNorm(256),
+            nn.LayerNorm([256, resolution // 8, resolution // 8]),  # Исправлено
             nn.LeakyReLU(0.2),
             nn.Dropout(dropout_prob),
 
             spectral_norm(nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1)),
-            nn.LayerNorm(512),
+            nn.LayerNorm([512, resolution // 16, resolution // 16]),  # Исправлено
             nn.LeakyReLU(0.2),
             nn.Dropout(dropout_prob),
 
             # Дополнительные сверточные слои
             spectral_norm(nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1)),
-            nn.LayerNorm(512),
+            nn.LayerNorm([512, resolution // 32, resolution // 32]),  # Исправлено
             nn.LeakyReLU(0.2),
             nn.Dropout(dropout_prob),
 
             spectral_norm(nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1)),
-            nn.LayerNorm(1024),
+            nn.LayerNorm([1024, resolution // 64, resolution // 64]),  # Исправлено
             nn.LeakyReLU(0.2),
             nn.Dropout(dropout_prob),
 
