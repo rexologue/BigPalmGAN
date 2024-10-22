7258bf36220df5473f952bebbee9982acfeea9a2
gregorymelekhov <mironovigoroffical@gmail.com>
2024-10-23 01:51:27 +0300

    .

--- biggan.py
+++ biggan.py
@@ -207,80 +207,54 @@ class BigGANBatchNorm(nn.Module):
 
 
 class GenBlock(nn.Module):
-    def __init__(self, in_size, out_size,
-                 condition_vector_dim,
-                 reduction_factor=4,
-                 up_sample=False,
-                 eps=1e-12):
+    def __init__(self, in_size, out_size, condition_vector_dim, reduction_factor=4, up_sample=False, eps=1e-12):
         super(GenBlock, self).__init__()
-        
         self.up_sample = up_sample
         self.drop_channels = (in_size != out_size)
         middle_size = in_size // reduction_factor
-        
-        self.bn_0 = BigGANBatchNorm(
-            in_size, condition_vector_dim, eps=eps, conditional=True)
-        
-        self.conv_0 = snconv2d(
-            in_channels=in_size,
-            out_channels=middle_size,
-            kernel_size=1, eps=eps)
-        
-        self.bn_1 = BigGANBatchNorm(
-            middle_size, condition_vector_dim, eps=eps, conditional=True)
-        
-        self.conv_1 = snconv2d(
-            in_channels=middle_size,
-            out_channels=middle_size,
-            kernel_size=3, padding=1, eps=eps)
-        
-        self.bn_2 = BigGANBatchNorm(
-            middle_size, condition_vector_dim, eps=eps, conditional=True)
-        
-        self.conv_2 = snconv2d(
-            in_channels=middle_size,
-            out_channels=middle_size,
-            kernel_size=3, padding=1, eps=eps)
-        
-        self.bn_3 = BigGANBatchNorm(
-            middle_size, condition_vector_dim, eps=eps, conditional=True)
-        
-        self.conv_3 = snconv2d(
-            in_channels=middle_size,
-            out_channels=out_size,
-            kernel_size=1, eps=eps)
-        
+
+        self.bn_0 = BigGANBatchNorm(in_size, condition_vector_dim, eps=eps, conditional=True)
+        self.conv_0 = snconv2d(in_channels=in_size, out_channels=middle_size, kernel_size=1, eps=eps)
+
+        self.bn_1 = BigGANBatchNorm(middle_size, condition_vector_dim, eps=eps, conditional=True)
+        self.conv_1 = snconv2d(in_channels=middle_size, out_channels=middle_size, kernel_size=3, padding=1, eps=eps)
+
+        self.bn_2 = BigGANBatchNorm(middle_size, condition_vector_dim, eps=eps, conditional=True)
+        self.conv_2 = snconv2d(in_channels=middle_size, out_channels=middle_size, kernel_size=3, padding=1, eps=eps)
+
+        self.bn_3 = BigGANBatchNorm(middle_size, condition_vector_dim, eps=eps, conditional=True)
+        self.conv_3 = snconv2d(in_channels=middle_size, out_channels=out_size, kernel_size=1, eps=eps)
+
         self.relu = nn.ReLU()
 
     def forward(self, x, cond_vector):
         x0 = x
-        
+
         x = self.bn_0(x, cond_vector)
         x = self.relu(x)
         x = self.conv_0(x)
+
         x = self.bn_1(x, cond_vector)
         x = self.relu(x)
-        
         if self.up_sample:
-            x = F.interpolate(
-                x, scale_factor=2, mode='nearest')
-            
+            x = F.interpolate(x, scale_factor=2, mode='nearest')
         x = self.conv_1(x)
+
         x = self.bn_2(x, cond_vector)
         x = self.relu(x)
         x = self.conv_2(x)
+
         x = self.bn_3(x, cond_vector)
         x = self.relu(x)
         x = self.conv_3(x)
-        
+
         if self.drop_channels:
             new_channels = x0.shape[1] // 2
             x0 = x0[:, :new_channels, ...]
             
         if self.up_sample:
-            x0 = F.interpolate(
-                x0, scale_factor=2, mode='nearest')
-            
+            x0 = F.interpolate(x0, scale_factor=2, mode='nearest')
+
         out = x + x0
         
         return out
@@ -288,52 +262,50 @@ class GenBlock(nn.Module):
 class Generator(nn.Module):
     def __init__(self, config):
         super(Generator, self).__init__()
-        
         self.config = config
         ch = config.channel_width
         condition_vector_dim = config.z_dim * 2
-        
-        self.gen_z = snlinear(
-            in_features=condition_vector_dim,
-            out_features=4 * 4 * 16 * ch, eps=config.eps)
-        
+
+        self.gen_z = snlinear(in_features=condition_vector_dim,
+                              out_features=4 * 4 * 16 * ch, eps=config.eps)
+
         layers = []
         for i, layer in enumerate(config.layers):
             if i == config.attention_layer_position:
-                layers.append(
-                    SelfAttn(ch*layer[1], eps=config.eps))
+                layers.append(SelfAttn(ch*layer[1], eps=config.eps))
                 
-            layers.append(
-                GenBlock(ch*layer[1], ch*layer[2],
-                         condition_vector_dim,
-                         up_sample=layer[0],
-                         eps=config.eps))
+            layers.append(GenBlock(ch*layer[1],
+                                   ch*layer[2],
+                                   condition_vector_dim,
+                                   up_sample=layer[0],
+                                   eps=config.eps))
             
         self.layers = nn.ModuleList(layers)
-        
+
         self.bn = BigGANBatchNorm(ch, eps=config.eps, conditional=False)
-        
         self.relu = nn.ReLU()
-        
-        self.conv_to_rgb = snconv2d(
-            in_channels=ch, out_channels=3,
-            kernel_size=3, padding=1, eps=config.eps)
-        
+        self.conv_to_rgb = snconv2d(in_channels=ch, out_channels=ch, kernel_size=3, padding=1, eps=config.eps)
         self.tanh = nn.Tanh()
 
-    def forward(self, cond_vector):
+    def forward(self, cond_vector, truncation):
         z = self.gen_z(cond_vector)
-        z = z.view(-1, 16 * self.config.channel_width, 4, 4)
-        
+
+        # We use this conversion step to be able to use TF weights:
+        # TF convention on shape is [batch, height, width, channels]
+        # PT convention on shape is [batch, channels, height, width]
+        z = z.view(-1, 4, 4, 16 * self.config.channel_width)
+        z = z.permute(0, 3, 1, 2).contiguous()
+
         for i, layer in enumerate(self.layers):
             if isinstance(layer, GenBlock):
-                z = layer(z, cond_vector)
+                z = layer(z, cond_vector, truncation)
             else:
                 z = layer(z)
-                
-        z = self.bn(z)
+
+        z = self.bn(z, truncation)
         z = self.relu(z)
         z = self.conv_to_rgb(z)
+        z = z[:, :3, ...]
         z = self.tanh(z)
         
         return z
